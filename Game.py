abc1b454dd4fe8fc44795359f26a61371ac3a81e
DGoblot <90686224+DGoblot@users.noreply.github.com>
2021-12-16 11:15:16 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,105 +1,128 @@
-class PlateauJ:
+class GameJ:
 
-    #La valeur 0 dans le plateau indique qu'il n'a pas de pion
-    #La valeur 1 c'est le peon du joueur 1
-    #La valeur -1 c'est le peon du joueur 2 / robot
+    # La valeur 0 dans le plateau indique qu'il n'a pas de pion
+    # La valeur 1 c'est le peon du joueur 1
+    # La valeur -1 c'est le peon du joueur 2 / robot
 
-    #player c'est pour indique qu'
+    # player c'est pour indique qu'
     def __init__(self):
-        self.plateau = [[0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0]]
+        self.plateau = [[0, 0, 0, 1, 0],
+                        [0, 0, 1, 0, 0],
+                        [0, 1, 0, 0, 0],
+                        [1, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 0]]
         self.player = 1
         self.pions_restant = 8
 
-
     def affichePl(self):
-        for i in range(0,5):
-            for j in range(0,5):
-                print(str(self.plateau[i][j])+" ", end =" ")
+        for i in range(0, 5):
+            for j in range(0, 5):
+                print(str(self.plateau[i][j]) + " ", end=" ")
 
             print("")
             print("--------------")
 
-    def switchJoeur(self):
-        if self.player==1:
-            self.player=-1
+    def switchJoueur(self):
+        if self.player == 1:
+            self.player = -1
         else:
-            self.player=1
+            self.player = 1
 
-
-    def mettrePions(self,x,y):
-        if(self.plateau[x][y]!=0):
-            return  False
-        else:
-            self.plateau[x][y]=self.player
-            self.switchJoeur()
-            self.pions_restant=self.pions_restant-1;
-            return  True
-
-    def movePions(self,ox,oy,nx,ny):
-        if self.plateau[ox][oy]==0 :
+    def mettrePions(self, x, y):
+        if (self.plateau[x][y] != 0):
             return False
         else:
-            if nx == ox + 1 or nx == ox - 1 or nx == ox:
-                if ny == oy or ny == oy + 1 or ny == oy - 1:
+            self.plateau[x][y] = self.player
+            self.switchJoueur()
+            self.pions_restant = self.pions_restant - 1
+        pass
 
-                    if self.plateau[nx][ny] == 0:
-                        self.plateau[ox][oy] = 0
-                        self.plateau[nx][ny] = self.player
-                        self.switchJoeur()
-                        return True
-                    else:
-                        return False
+    def movePions(self):
+        pass
 
-                else:
-                    return False
+    def gagner(self, x, y):#fonction vérifiant si le pion posé déclanche une victoire
+        win = False
+        colonne = [self.plateau[i][y] for i in range(5)]
+        verif = [1, 1, 1, 1]
+        verifCarre = [1, 1]
+        if (self.player == -1):
+            verif = [-1, -1, -1, -1]
+            verifCarre = [-1, -1]
+
+        print(verif)
+
+        if (self.contient(verif, self.plateau[x])):#vérification d'une victoire en ligne
+            win = True
+
+        elif (self.contient(verif, colonne)):#vérification d'une victoire en colonne
+            win = True
+            print(self.contient(verif, colonne))
+
+        elif (self.contient(verifCarre, self.plateau[x])):#vérification d'une victoire en carré
+            carreInfo1 = self.contient(verifCarre, self.plateau[x])
+            if (x != 0):
+                carreInfo2 = self.contient(verifCarre, self.plateau[x-1])
+                if (carreInfo2 and carreInfo1[1] == carreInfo2[1]):
+                    win = True
+            if (x != len(self.plateau) - 1):
+                carreInfo2 = self.contient(verifCarre, self.plateau[x+1])
+                if (carreInfo2 and carreInfo1[1] == carreInfo2[1]):
+                    win = True
+        else:#verification d'une victoire en diagonale
+            if(x == y):
+                if(self.contient(verif,[diag[i] for i, diag in enumerate(self.plateau)])):
+                    win = True
+            elif(x+y == 4):
+                if(self.contient(verif,[diag[-i-1] for i, diag in enumerate(self.plateau)])):#vérification sur les deux grandes diagonales
+                    win = True
+            elif(x == y-1):
+                petiteDiag = []
+                for i in range(4):
+                    petiteDiag.append(self.plateau[i][i+1])
+                    if(self.contient(verif,petiteDiag)):
+                        win = True
+            elif(x == y+1):
+                petiteDiag = []
+                for i in range(4):
+                     petiteDiag.append(self.plateau[i][i])
+                     if(self.contient(verif,petiteDiag)):
+                           win = True
+            elif(x+y == 3):
+                petiteDiag = []
+                for i in range(4):
+                    petiteDiag.append(self.plateau[3-i][i])
+                    if(self.contient(verif,petiteDiag)):
+                        win = True
+            elif(x+y == 5):
+                petiteDiag = []
+                for i in range(4):
+                    petiteDiag.append(self.plateau[4-i][i+1])
+                    if(self.contient(verif,petiteDiag)):
+                        win = True
+
+        if (win == True):
+            return self.player
+        else:
+            return 0
+
+    def contient(self, petite, grande):
+        for i in range(len(grande) - len(petite) + 1):
+            for j in range(len(petite)):
+                if grande[i + j] != petite[j]:
+                    break
             else:
-                return False
+                return True, i
+        return False
 
-
-
+    def presquegagner(self):
+        pass
 
     def jouer(self):
-        while True:
-            p.affichePl()
-            print("Le joueur numero va jouer "+str(self.player))
-            if (self.pions_restant == 0):
-                print("Quelle pion modifier la position")
-                while True:
-                    print("Donner un ox et un oy")
-
-                    ox = int(input())
-                    oy = int(input())
-                    if (ox >= 0 and ox < 5) and (oy >= 0 and oy < 5) and self.plateau[ox][oy]==self.player:
-                        break
-                    else:
-                        print("Vous avez donner un ox et un oy faux")
-                print("Quelle futur position")
-                while True:
-                    print("Donner un nx et un ny")
-                    nx = int(input())
-                    ny = int(input())
-                    if (nx >= 0 and nx < 5) and (ny >= 0 and ny < 5) and self.movePions(ox,oy,nx,ny) :
-                        break
-                    else:
-                        print("Vous avez donner un nx et un ny faux")
+        pass
 
 
-            else:
-                while True:
-                    print("Donner un x et un y")
-
-                    x = int(input())
-                    y = int(input())
-                    if (x >= 0 and x < 5) and (y >= 0 and y < 5) and self.mettrePions(x,y):
-                        break
-                    else:
-                        print("Vous avez donner un x et un y faux")
-
-
-p=PlateauJ()
-
-p.jouer()
+test = GameJ()
+test.__init__()
+print(test.gagner(1, 2))
+# p=PlateauJ()
+# p.affichePl()
