6da982df4d5d5ffa33b04f94957eab3ba6c5767a
DGoblot <90686224+DGoblot@users.noreply.github.com>
2022-01-01 17:20:47 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,31 +1,33 @@
-import time
-
-
 class GameJ:
 
     def __init__(self):
-        self.plateau = [[0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0]]
+        self.plateau = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]]
         self.player = 1
         self.pions_restant = 8
         self.phase = 'Mettre'
-        self.temp = 0,0
+        self.temp = 0, 0
+        self.visualisation = [[0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0]]
 
     def switchJoeur(self):
-        if self.player==1:
-            self.player=-1
+        if self.player == 1:
+            self.player = -1
         else:
-            self.player=1
+            self.player = 1
 
-    def mettrePions(self,x,y):
-        if(self.plateau[x][y]!=0):
-            return  False
+    def mettrePions(self, x, y):
+        if (self.plateau[x][y] != 0):
+            return False
         else:
-            self.plateau[x][y]=self.player
-            self.pions_restant=self.pions_restant-1
+            self.plateau[x][y] = self.player
+            self.pions_restant = self.pions_restant - 1
             if self.pions_restant <= 1:
                 self.verifVictoire(x, y)
             self.switchJoeur()
@@ -37,7 +39,11 @@ class GameJ:
         if self.plateau[ox][oy] != self.player:
             return False
         else:
-            self.temp = ox,oy
+            self.temp = ox, oy
+            for i in range(max(0, ox - 1), min(ox + 1, 4) + 1):
+                for j in range(max(0, oy - 1), min(oy + 1, 4) + 1):
+                    if self.plateau[i][j] == 0:
+                        self.visualisation[i][j] = 1
             self.phase = 'movePoser'
             return True
 
@@ -45,7 +51,7 @@ class GameJ:
         if nx == self.temp[0] and ny == self.temp[1]:
             self.phase = 'movePrendre'
             return True
-        elif self.plateau[nx][ny]!=0:
+        elif self.plateau[nx][ny] != 0:
             return False
         else:
             if nx == self.temp[0] + 1 or nx == self.temp[0] - 1 or nx == self.temp[0]:
@@ -53,6 +59,9 @@ class GameJ:
                     self.plateau[self.temp[0]][self.temp[1]] = 0
                     self.plateau[nx][ny] = self.player
                     self.verifVictoire(nx, ny)
+                    for i in range(5):
+                        for j in range(5):
+                            self.visualisation[i][j] = 0
                     self.phase = 'movePrendre'
                     self.switchJoeur()
                     return True
@@ -63,11 +72,11 @@ class GameJ:
 
     def verifVictoire(self, x, y):
         verif = self.gagner(x, y)
-        if(verif[0]):
+        if (verif[0]):
             print("Le joueur " + str(verif[1]) + " a gagné.")
             exit()
 
-    def gagner(self, x, y):#fonction vérifiant si le pion posé déclanche une victoire
+    def gagner(self, x, y):  # fonction vérifiant si le pion posé déclanche une victoire
         win = False
         colonne = [self.plateau[i][y] for i in range(5)]
 
@@ -78,51 +87,52 @@ class GameJ:
             verif = [1, 1, 1, 1]
             verifCarre = [1, 1]
 
-        if (self.contient(verif, self.plateau[x])):#vérification d'une victoire en ligne
+        if (self.contient(verif, self.plateau[x])):  # vérification d'une victoire en ligne
             win = True
-        elif (self.contient(verif, colonne)):#vérification d'une victoire en colonne
+        elif (self.contient(verif, colonne)):  # vérification d'une victoire en colonne
             win = True
 
-        elif (self.contient(verifCarre, self.plateau[x])):#vérification d'une victoire en carré
+        elif (self.contient(verifCarre, self.plateau[x])):  # vérification d'une victoire en carré
             carreInfo1 = self.contient(verifCarre, self.plateau[x])
             if (x != 0):
-                carreInfo2 = self.contient(verifCarre, self.plateau[x-1])
+                carreInfo2 = self.contient(verifCarre, self.plateau[x - 1])
                 if (carreInfo2 and carreInfo1[1] == carreInfo2[1]):
                     win = True
             if (x != len(self.plateau) - 1):
-                carreInfo2 = self.contient(verifCarre, self.plateau[x+1])
+                carreInfo2 = self.contient(verifCarre, self.plateau[x + 1])
                 if (carreInfo2 and carreInfo1[1] == carreInfo2[1]):
                     win = True
-        else:#verification d'une victoire en diagonale
-            if(x == y):
-                if(self.contient(verif,[diag[i] for i, diag in enumerate(self.plateau)])):
+        else:  # verification d'une victoire en diagonale
+            if (x == y):
+                if (self.contient(verif, [diag[i] for i, diag in enumerate(self.plateau)])):
                     win = True
-            if(x+y == 4):
-                if(self.contient(verif,[diag[-i-1] for i, diag in enumerate(self.plateau)])):#vérification sur les deux grandes diagonales
+            if (x + y == 4):
+                if (self.contient(verif, [diag[-i - 1] for i, diag in
+                                          enumerate(self.plateau)])):  # vérification sur les deux grandes diagonales
                     win = True
-            if(x == y-1):
+            if (x == y - 1):
                 petiteDiag = []
                 for i in range(4):
-                    petiteDiag.append(self.plateau[i][i+1])
-                if(self.contient(verif,petiteDiag)):
+                    petiteDiag.append(self.plateau[i][i + 1])
+                if (self.contient(verif, petiteDiag)):
                     win = True
-            if(x == y+1):
+            if (x == y + 1):
                 petiteDiag = []
                 for i in range(4):
-                    petiteDiag.append(self.plateau[i+1][i])
-                if(self.contient(verif,petiteDiag)):
+                    petiteDiag.append(self.plateau[i + 1][i])
+                if (self.contient(verif, petiteDiag)):
                     win = True
-            if(x+y == 3):
+            if (x + y == 3):
                 petiteDiag = []
                 for i in range(4):
-                    petiteDiag.append(self.plateau[3-i][i])
-                if(self.contient(verif,petiteDiag)):
+                    petiteDiag.append(self.plateau[3 - i][i])
+                if (self.contient(verif, petiteDiag)):
                     win = True
-            if(x+y == 5):
+            if (x + y == 5):
                 petiteDiag = []
                 for i in range(4):
-                    petiteDiag.append(self.plateau[4-i][i+1])
-                if(self.contient(verif,petiteDiag)):
+                    petiteDiag.append(self.plateau[4 - i][i + 1])
+                if (self.contient(verif, petiteDiag)):
                     win = True
 
         if (win == True):
@@ -138,3 +148,30 @@ class GameJ:
             else:
                 return True, i
         return False
+
+    def getAdjacent(self, l, c):
+        adjacents = []
+        directions = [
+            [-1, -1], [-1, 0], [-1, +1],
+            [0, -1], [0, +1],
+            [+1, -1], [+1, 0], [+1, +1],
+        ]
+
+        for i in directions:
+            if (0 <= l + i[0] <= 4 and 0 <= c + i[1] <= 4):
+                adjacents.append([l + i[0], c + i[1]])
+        return adjacents
+
+    # le nombre de pion adjacent du meme joueur
+    def countAdjacent(self, l, c, type):
+        val = 1
+        s = 0;
+        if not (type):
+            val = -1
+
+        adjacents = self.getAdjacent(l, c)
+
+        for ad in adjacents:
+            if self.plateau[ad[0]][ad[1]] == val:
+                s = s + 1
+        return s
--- UI.py
+++ UI.py
@@ -1,37 +1,73 @@
 from tkinter import *
 import Game
+import IA
 
 
 class UIJ:
     def __init__(self):
         self.jeu = Game.GameJ()
         self.fen = Tk()
+        self.fen.eval('tk::PlaceWindow . center')
         self.fen.title('TEEKO')
-        self.can = None
+        self.can = Canvas(self.fen, height=500, width=500)
+        self.niveau = 0
+        self.slider = Scale(self.fen, from_=0, to=2, orient=HORIZONTAL, length=300, sliderlength=40, showvalue=False)
+        self.ia = IA.Ia(self.jeu, -1, 1)
 
     def menu(self):
-        lab=Label(self.fen,text="Bienvenue sur le projet TEEKO d'IA41 de"+'\n'+"Youssef , David , Nisrine et Mouad",font=('Arial',16,'bold'))
-        lab.pack(side=TOP,padx=15,pady=15)#On crée une étiquette (un texte) de presentation
+        lab = Label(self.fen,
+                    text="Bienvenue sur le projet TEEKO d'IA41 de" + '\n' + "Youssef , David , Nisrine et Mouad",
+                    font=('Arial', 16, 'bold'))
+        lab.pack(side=TOP, padx=15, pady=15)  # On crée une étiquette (un texte) de presentation
 
-        bou1 = Button(self.fen, text='Joueur VS Joueur', font=('Arial',15,'normal'), padx=25, pady=5, command=self.jeuUIsolo)
-        bou1.pack(side=LEFT,padx=10,pady=4)#On crée un bouton pour lancer une partie Joueur contre Joueur
+        bou1 = Button(self.fen, text='Joueur VS Joueur', font=('Arial', 15, 'normal'), padx=25, pady=5,
+                      command=self.jeuUIsolo)
+        bou1.pack(side=LEFT, padx=10, pady=4)  # On crée un bouton pour lancer une partie Joueur contre Joueur
 
-        bou2 = Button(self.fen, text='Joueur VS Ordinateur', font=('Arial', 15, 'normal'), padx=10, pady=5, command=self.jeuUIia)
-        bou2.pack(side=RIGHT,padx=10,pady=4)
+        bou2 = Button(self.fen, text='Joueur VS Ordinateur', font=('Arial', 15, 'normal'), padx=10, pady=5,
+                      command=self.selectIA)
+        bou2.pack(side=RIGHT, padx=10, pady=4)
+
+        bou3 = Button(self.fen, text='Ordinateur VS Ordinateur', font=('Arial', 15, 'normal'), padx=10, pady=5,
+                      command=self.selectIA)
+        bou3.pack(side=RIGHT, padx=10, pady=4)
+
         self.fen.mainloop()
+
+    def selectIA(self):
+        liste = self.fen.pack_slaves()
+        for i in range(len(liste)):
+            liste[i].destroy()
+        lab = Label(self.fen,text="Choisissez votre difficulté",font=('Arial', 16, 'bold'))
+        lab.pack(side=TOP, padx=15, pady=15)
+
+        fen_lab = Frame(self.fen)
+
+        lab1 = Label(fen_lab, text="Très facile",font=('Arial', 12), padx=15)
+        lab2 = Label(fen_lab, text="Facile",font=('Arial', 12), padx=15)
+        lab3 = Label(fen_lab, text="Normal",font=('Arial', 12), padx=15)
+        lab4 = Label(fen_lab, text="Difficile",font=('Arial', 12), padx=15)
+        lab1.pack(side=LEFT)
+        lab2.pack(side=LEFT)
+        lab3.pack(side=LEFT)
+        lab4.pack(side=LEFT)
+
+        fen_lab.pack()
+
+        self.slider.pack()
+
+        bou = Button(self.fen, text='Confirmer', font=('Arial', 15, 'normal'), padx=25, pady=5,
+                     command=self.jeuUIia)
+        bou.pack(side=BOTTOM, padx=10, pady=4)
 
     def jeuUIsolo(self):
         liste = self.fen.pack_slaves()
         for i in range(len(liste)):
             liste[i].destroy()
-        self.can = Canvas(self.fen, height=500, width=500)
         self.can.pack()
         self.terrain()
         self.can.focus_set()
         self.can.bind('<Button-1>', self.clique)
-
-    def jeuUIia(self):
-        print("cool")
 
     def clique(self, event):
         x = int(event.x / 100)
@@ -39,9 +75,34 @@ class UIJ:
         if (self.jeu.phase == 'Mettre'):
             self.jeu.mettrePions(x, y)
         elif self.jeu.phase == 'movePrendre':
-            self.jeu.movePionsPrendre(x,y)
+            self.jeu.movePionsPrendre(x, y)
         elif self.jeu.phase == 'movePoser':
-            self.jeu.movePionsPoser(x,y)
+            self.jeu.movePionsPoser(x, y)
+        self.update()
+
+    def jeuUIia(self):
+        self.niveau = self.slider.get()
+        liste = self.fen.pack_slaves()
+        for i in range(len(liste)):
+            liste[i].destroy()
+        self.can.pack()
+        self.terrain()
+        self.can.focus_set()
+        self.can.bind('<Button-1>', self.cliqueIA)
+
+    def cliqueIA(self, event):
+        x = int(event.x / 100)
+        y = int(event.y / 100)
+        if (self.jeu.phase == 'Mettre'):
+            self.jeu.mettrePions(x, y)
+        elif self.jeu.phase == 'movePrendre':
+            self.jeu.movePionsPrendre(x, y)
+        elif self.jeu.phase == 'movePoser':
+            self.jeu.movePionsPoser(x, y)
+        self.update()
+        if self.jeu.phase != 'movePrendre':
+            if self.niveau == 0:
+                self.ia.niveauFacile()
         self.update()
 
     def terrain(self):
@@ -57,10 +118,13 @@ class UIJ:
             for j in range(5):
                 if self.jeu.plateau[i][j] == 1:
                     self.can.create_oval(20 + (i * 100), 20 + (j * 100), 80 + (i * 100), 80 + (j * 100), width=5,
-                                         outline='blue',fill='blue')
+                                         outline='blue', fill='blue')
                 if self.jeu.plateau[i][j] == -1:
                     self.can.create_oval(20 + (i * 100), 20 + (j * 100), 80 + (i * 100), 80 + (j * 100), width=5,
-                                     outline='red',fill='red')
+                                         outline='red', fill='red')
+                if self.jeu.visualisation[i][j] == 1:
+                    self.can.create_oval(20 + (i * 100), 20 + (j * 100), 80 + (i * 100), 80 + (j * 100), width=5,
+                                         outline='green')
 
 
 ui = UIJ()
